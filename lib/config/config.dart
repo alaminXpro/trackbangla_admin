cd434e2a4f91f4daa434406e52c082b833c24cc2
alaminXpro <alaminxpro@gmail.com>
2024-01-08 13:44:00 +0600

    Commit from AlaminXpro

---
+++ lib/config/config.dart
@@ -0,0 +1,8 @@
+class Config{
+  final String appName = 'TrackBangla'; //app name
+  final String testerPassword = 'tester12345';  //testing password
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAAtNTQzkg:APA91bH-GBF-U_wA5lkP1Yc3aTEdtK-ZwBGfv7i2D2H7EpKBxYE2FFKdT3kaxDfTbyk6jqA8Hdfmz7nVZKKOGFtaKyzEDyve_MDbIfQH4Yqg5Yp5RQnHUs6Ms7LQX8dC8CtBiU_1CzK_';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
